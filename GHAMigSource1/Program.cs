19429f7c84978dbaffcee3552c1370130d7a753e
Rithuraj Sunilkumar <rithurajsun47@gmail.com>
2025-11-04 16:57:41 +0530

    Bug fix 1

--- GHAMigSource1/Program.cs
+++ GHAMigSource1/Program.cs
@@ -4,13 +4,13 @@
     {
         static void Main(string[] args)
         {
-            int a,b;
+            int a, b;
             Console.WriteLine("Enter the values of a and b");
             a = int.Parse(Console.ReadLine());
             b = int.Parse(Console.ReadLine());
             int sum = a + b;
 
-            Console.WriteLine($"The sum of two number is {sum}")
+            Console.WriteLine($"The sum of two number is {sum}");
         }
     }
 }
